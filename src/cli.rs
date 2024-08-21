78c65f816e8c1035ab5133b70c710b67c575302e
Ale David Weber <Alexis.Weber@mailfence.com>
2024-08-21 18:29:35 -0300

    initialize app

---
+++ src/cli.rs
@@ -0,0 +1,29 @@
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+pub enum Action {
+    /// Write tasks to the journal file.
+    Add {
+        #[structopt()]
+        task: String
+    },
+    /// Remove an entryfrom the jornal file by posotion. 
+    Done {
+        #[structopt()]
+        position: usize
+    },
+    /// List all tasks in the journal file.
+    List,
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(name= "Daily Tasks", about = "A command line to-do app written in Rust")]
+pub struct CommandLineArgs {
+    #[structopt(subcommand)]
+    pub action: Action,
+    
+    /// Use a different journal file.
+    #[structopt(parse(from_os_str), short, long)]
+    pub journal_file: Option<PathBuf>,
+}
